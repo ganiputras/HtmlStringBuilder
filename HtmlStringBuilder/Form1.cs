93f5617e5c05278f0d8fd3b8daba5110638127ba
ganiputras <ganiputras@gmail.com>
2025-05-16 13:33:40 +0700

    revision

--- HtmlStringBuilder/Form1.cs
+++ HtmlStringBuilder/Form1.cs
@@ -13,16 +13,27 @@ namespace HtmlStringBuilder
         private void button1_Click(object sender, EventArgs e)
         {
             String s = String.Empty;
-
+            s += "private string BodyHtml(){" + Environment.NewLine;
             s += "var sb = new StringBuilder();" + Environment.NewLine;
 
+            string _ = "\"";
+            string cs = "\\\"";
             foreach (var t in richTextBox1.Lines)
             {
-                var text = $"sb.AppendLine({t})";
+                if (string.IsNullOrWhiteSpace(t))
+                {
+                    continue;
+                }
+
+                var rpl = t.Replace(_, cs);
+                var value = $"{_}{rpl}{_}".ToString();
+                var text = $"sb.AppendLine({value});";
                 s += text + Environment.NewLine;
                 //s += text + Environment.NewLine;
             }
             s += "return sb.ToString();" + Environment.NewLine;
+
+            s += "}" + Environment.NewLine;
 
             richTextBox2.Text = s;
         }
